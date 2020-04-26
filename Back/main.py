50062845cc76c00fa8c09bc9ec3717f25e2d1197
kasimirova <kasimirova@users.noreply.github.com>
2020-04-26 16:56:17 +0700

    back

--- Back/main.py
+++ Back/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import pandas as pd
-from flask_session import Session
 #from werkzeug import secure_filename
-from flask import g
 app = Flask(__name__)
-sess = Session()
 
 @app.route("/", methods=['GET','POST'])
 def upload():
@@ -12,7 +9,6 @@ def upload():
     if request.method == 'POST':
         f = request.files['fileU']
         file_name = f.filename
-        g.data = file_name
         f.save('C:\\Users\\Дарья\\Desktop\\Диплом\\diplomProject\\Back\\' + file_name)
         return redirect(url_for('showTable', error=error, file=file_name))
     # else:
@@ -25,19 +21,26 @@ def uploadSucc():
         return "Успешно."
     return "Успешно."
 
-@app.before_request
-def before_request():
-    g.field = get_field(NUM)
-#https://ru.stackoverflow.com/questions/779534/%D0%9F%D0%B5%D1%80%D0%B5%D0%B4%D0%B0%D1%87%D0%B0-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D0%BC%D0%B5%D0%B6%D0%B4%D1%83-%D0%B2%D1%8C%D1%8E%D1%88%D0%BA%D0%B0%D0%BC%D0%B8-python-flask
+def GetDictFromPandas(data):
+    cols = data.shape[1]
+    rows = data.shape[0]
+    print(cols)
+    d = {}
+    for i in range(rows):
+        lst = []
+        for j in range(cols):
+            lst.append(data.iloc[i, j])
+        d[i] = lst
+    return d
+
+
 @app.route("/showTable")
 def showTable():
-    fileName = getattr(g, 'data', None)
-    data = pd.read_csv('titanic.csv', header=None)
-    print(fileName)
-    c = data.count
-    names = data.iloc[0, :]
-    data = data.iloc[1:, :]
-    return render_template('showtable.html', data=data.iloc[0, :], name=names, row=c)
+    fileName = request.args.get('file')
+    #data = pd.read_csv('data-logistic.csv', header=None)
+    data = pd.read_csv(fileName, header=None)
+    dct = GetDictFromPandas(data)
+    return render_template('showtable.html', data=dct)
 
 
 if __name__ == "__main__":
