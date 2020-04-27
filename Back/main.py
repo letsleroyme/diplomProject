e10b530bb657c72869272c6e7ecc4b5093c56a75
kasimirova <kasimirova@users.noreply.github.com>
2020-04-27 21:49:59 +0700

    back

--- Back/main.py
+++ Back/main.py
@@ -1,30 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
+
 #from werkzeug import secure_filename
 app = Flask(__name__)
 
+CORS(app)
+
 @app.route("/", methods=['GET','POST'])
 def upload():
-    error = None
     if request.method == 'POST':
-        f = request.files['fileU']
-        file_name = f.filename
-        f.save('C:\\Users\\Дарья\\Desktop\\Диплом\\diplomProject\\Back\\' + file_name)
-        return redirect(url_for('showTable', error=error, file=file_name))
-    # else:
-    #     return "Ошибка"
-    return render_template('upload.html', error=error)
-    
-@app.route("/uploadSucc", methods=['GET','POST'])
-def uploadSucc():
-    if request.method == 'POST':
-        return "Успешно."
-    return "Успешно."
+        f = request.files['file']
+        fileName = f.filename
+        f.save('C:\\Users\\Дарья\\Desktop\\Диплом\\diplomProject\\Back\\' + fileName)
+        data = pd.read_csv(fileName, header=None)
+        dct = GetDictFromPandas(data)
+        return jsonify(dct)
 
 def GetDictFromPandas(data):
     cols = data.shape[1]
     rows = data.shape[0]
-    print(cols)
     d = {}
     for i in range(rows):
         lst = []
@@ -32,15 +27,6 @@ def GetDictFromPandas(data):
             lst.append(data.iloc[i, j])
         d[i] = lst
     return d
-
-
-@app.route("/showTable")
-def showTable():
-    fileName = request.args.get('file')
-    #data = pd.read_csv('data-logistic.csv', header=None)
-    data = pd.read_csv(fileName, header=None)
-    dct = GetDictFromPandas(data)
-    return render_template('showtable.html', data=dct)
 
 
 if __name__ == "__main__":
