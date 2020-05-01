47ab1ecbddc065131c2bf96057b0d2fc406c7183
kasimirova <kasimirova@users.noreply.github.com>
2020-05-01 20:49:52 +0700

    back

--- Back/main.py
+++ Back/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
 
 #from werkzeug import secure_filename
 app = Flask(__name__)
@@ -9,23 +10,31 @@ CORS(app)
 
 @app.route("/", methods=['GET','POST'])
 def upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        fileName = f.filename
-#         f.save('/home/anatoly/diplomProject/Back/' + fileName)
-        f.save('/home/anatoly/diplomProject/Back/' + fileName)
-        data = pd.read_csv(fileName, header=None)
-        dct = GetDictFromPandas(data)
-        return jsonify(dct)
+    f = request.files['file']
+    fileName = f.filename
+    formt = fileName.split(".")
+    if formt[1] != 'csv':
+        response = jsonify({'status': 415, 'error': 'Unsupported Media Type',
+                            'message': 'Wrong type of file'})
+        response.status_code = 415
+        return response
+    f.save('C:\\Users\\Дарья\\Desktop\\Диплом\\diplomProject\\Back\\' + fileName)
+    data = pd.read_csv(fileName, header=None)
+    dct = GetDictFromPandas(data)
+    return jsonify(dct)
+
 
 def GetDictFromPandas(data):
     cols = data.shape[1]
     rows = data.shape[0]
+    a =[c for c in range(1, cols+1)]
     d = {}
-    for i in range(rows):
+    d[0] = a
+    for i in range(1, rows+1):
         lst = []
+        lst.append(i)
         for j in range(cols):
-            lst.append(data.iloc[i, j])
+            lst.append(data.iloc[i-1, j])
         d[i] = lst
     return d
 
