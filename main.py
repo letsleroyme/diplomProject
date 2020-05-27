39a47f20a9d2b9c8b22b1290e9c581aca6c63af6
letsleroyme <letsleroyme@gmail.com>
2020-05-27 23:02:07 +0700

    cors another time

--- main.py
+++ main.py
@@ -19,6 +19,15 @@ class Singleton(object):
     header = None
     filename = None
 
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
 @app.route("/", methods=['POST'])
 @crossdomain(origin='*')
 def upload():
