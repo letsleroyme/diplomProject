9800470d416a4ba8dd1e99060a0ec34ec7d9670e
letsleroyme <letsleroyme@gmail.com>
2020-05-27 20:00:00 +0700

    added some headers

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ CORS(app, resources={
     }
 })
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 class Singleton(object):
     def __new__(cls):
