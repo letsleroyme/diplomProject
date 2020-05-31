060af6634d4a1af2bfadf592204016b422c00bf2
kasbitch <44318531+kasbitch@users.noreply.github.com>
2020-05-31 19:24:48 +0700

    Update ProcessData.py

--- ProcessData.py
+++ ProcessData.py
@@ -220,21 +220,70 @@ def GetDataForCharts(requestData, data):
         responseDct['header'] = headDct
     return responseDct
 
-def MakeARequest(requestData, data, header):
-    colnum = (int)(requestData['column']) - 1
-    value =(requestData['value'])
+
+def ccs(col, op, value, data):
     variation = {
-        '=': data[(data.iloc[:, colnum] == value)],
-        '>': data[(data.iloc[:, colnum] > value)],
-        '<': data[(data.iloc[:, colnum] < value)],
-        '>=': data[((data.iloc[:, colnum]) >= value)],
-        '<=': data[(data.iloc[:, colnum] <= value)],
-        '!=': data[(data.iloc[:, colnum] != value)],
+        '=': data[(data.iloc[:, col] == value)],
+        '>': data[(data.iloc[:, col] > value)],
+        '<': data[(data.iloc[:, col] < value)],
+        '>=': data[(data.iloc[:, col] >= value)],
+        '<=': data[(data.iloc[:, col] <= value)],
+        '!=': data[(data.iloc[:, col] != value)],
     }
-    dataAfterRequest = variation[requestData['operator']]
-    dct = GetDictTable(pd.concat([header, dataAfterRequest], axis=0))
+    return variation[op]
+
+def GetType(listOfConj):
+    outstr = ''
+    for i in listOfConj:
+        outstr = outstr + i[0]
+    return outstr
+
+def MakeARequest(requestData, data, header):
+    listOfConj = [(key.split("e")[2])[:-1] for key, value in requestData.items() if key.startswith('is') and value]
+    listOfCol = [(int)(value) - 1 for key, value in requestData.items() if key.startswith('col') and value]
+    listOfOperators = [value for key, value in requestData.items() if key.startswith('op') and value]
+    listOfValues = [value for key, value in requestData.items() if key.startswith('va') and value]
+    strType = GetType(listOfConj)
+    newd = data.copy()
+    ndata = data.copy()
+    subs = [a for a in range(newd.shape[0])]
+    if strType == 'AA':
+        newd = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], newd)
+        newd = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], newd)
+        newd = ccs(listOfCol[2], listOfOperators[2], listOfValues[2], newd)
+    if strType == 'AO':
+        ndata = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], ndata)
+        ndata = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], ndata)
+        forOr = ccs(listOfCol[2], listOfOperators[2], listOfValues[2], data)
+        newd = pd.concat([ndata, forOr], axis=0)
+        newd = newd.drop_duplicates(subset=subs)
+    if strType == 'OA':
+        ndata = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], ndata)
+        ndata = ccs(listOfCol[2], listOfOperators[2], listOfValues[2], ndata)
+        forOr = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], data)
+        newd = pd.concat([ndata, forOr], axis=0)
+        newd = newd.drop_duplicates(subset=subs)
+    if strType == 'OO':
+        ndata1 = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], data)
+        ndata2 = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], data)
+        ndata3 = ccs(listOfCol[2], listOfOperators[2], listOfValues[2], data)
+        newd = pd.concat([ndata1, ndata2, ndata3], axis=0)
+        newd = newd.drop_duplicates(subset=subs)
+    if strType == 'A':
+        newd = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], newd)
+        newd = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], newd)
+    if strType == 'O':
+        forOr = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], data)
+        forOr2 = ccs(listOfCol[1], listOfOperators[1], listOfValues[1], data)
+        newd = pd.concat([forOr, forOr2], axis=0)
+        newd = newd.drop_duplicates(subset=subs)
+
+    if not len(strType):
+        newd = ccs(listOfCol[0], listOfOperators[0], listOfValues[0], newd)
+
+    dct = GetDictTable(pd.concat([header, newd], axis=0))
     response = {}
-    response['amountOfStr'] = dataAfterRequest.shape[0]
+    response['amountOfStr'] = newd.shape[0] -1
     response['data'] = dct
     print(response)
     return response
