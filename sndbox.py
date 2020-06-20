4b3588785b85e8f731f72e37537a3af92ebb38bd
kasbitch <44318531+kasbitch@users.noreply.github.com>
2020-06-20 21:51:01 +0700

    Update sndbox.py

--- sndbox.py
+++ sndbox.py
@@ -141,9 +141,6 @@ def json_finalizer(executor):
 
 
 def mainFunk(filename, ListOfStr):
-    data = pd.read_csv(filename)
-    print(data)
     for n in ListOfStr:
         f = exec_str(n, json_finalizer)
-        print(f)
     return f
